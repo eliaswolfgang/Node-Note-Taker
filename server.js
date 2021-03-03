bdc16cbbb4c9800162b35a92d4c20c7eb322b789
Ian Von Fange <wolfgangvonfange@gmail.com>
2021-03-02 21:29:23 -0600

    Added delete button

--- server.js
+++ server.js
@@ -2,15 +2,15 @@ const express = require('express');
 const fs = require('fs');
 const path = require('path');
 const app = express();
-const { nanoid } = require('nanoid');
+const { nanoid } = require('nanoid'); // For random ID generation upon save
 
 const PORT = process.env.PORT || 5000;
-
+// Middlewares
 app.use(express.urlencoded({ extended: true }));
 app.use(express.static('public'));
 app.use(express.json());
 
-// GET "*" returns index.html.
+// GET "/" returns index.html.
 app.get('/', (req, res) => {
     res.sendFile(path.join(__dirname, 'public/index.html'))
 });
@@ -25,6 +25,7 @@ app.get('/api/notes', (req, res) => {
     res.sendFile(path.join(__dirname, '/db/db.json'))
 });
 
+// Save a new note 
 app.post('/api/notes', (req, res) => {
     const newNote = {
         id: nanoid(5),
@@ -38,18 +39,27 @@ app.post('/api/notes', (req, res) => {
         notesArr.push(newNote);
         notesObj = JSON.stringify(notesArr, null, 2);
         fs.writeFile('./db/db.json', notesObj, (err) => {
-            if (err) {
-                console.log(err)
-            }
+            if (err) { console.log(err) }
         });
     });
-    res.sendFile(path.join(__dirname, './db/db.json'));    
+    res.sendFile(path.join(__dirname, './db/db.json'));
+});
+// Delete a note
+app.delete('/api/notes/:id', (req, res) => {
+    fs.readFile('./db/db.json', (err, data) => {
+        const noteID = req.params.id;
+        let notesObj;
+        let notesArr = JSON.parse(data);
+        const note = notesArr.find( ({ id }) => id == noteID);
+        if (!note) { res.status(400).send(`The note with the ID of ${id} does not exist`) }
+        const index = notesArr.indexOf(note);
+        notesArr.splice(index, 1);
+        notesObj = JSON.stringify(notesArr, null, 2);
+        fs.writeFile('./db/db.json', notesObj, (err) => {
+            if (err) { console.log(err) }
+        });
+    });
+    res.sendFile(path.join(__dirname, './db/db.json'));
 });
 
-
 app.listen(PORT, () => console.log(`Server started on ${PORT}`));
-
-
-
-// const html = fs.readFileSync( __dirname + '/main.html' );
-// res.json({html: html.toString(), data: obj});
